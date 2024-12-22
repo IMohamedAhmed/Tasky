4ee7bbeecc118c5ef73106f1c56aee18a013ae96
I1MohamedAhmed <email-for-first-account@example.com>
2024-12-23 00:47:36 +0200

    added authorization and authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const TasksController = require("../controllers/Task");
+const UsersController = require("../controllers/User");
 
 //tasks
 router.get("/tasks", TasksController.getTasks);
 router.post("/task", TasksController.createTask);
 
-// router.get("/tasks");
-// router.post("/task");
+//users
+router.post("/user/sign-in", UsersController.login);
+router.post("/user/sign-up", UsersController.register);
 
 // router.get("/tasks");
 // router.post("/task");
